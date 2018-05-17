06e4b797db1a93b96ceba3346f9e9270addf7fa2
chuiboy <34258436+chuiboy@users.noreply.github.com>
2018-05-18 00:09:53 +1000

    Update game.py
    Changed it so a reward is returned no matter the action.

--- GridWorld/game.py
+++ GridWorld/game.py
@@ -16,22 +16,22 @@ class Game:
         if action == 0: # Up
             if self.currentState not in [0, 1, 2, 8, 4, 11]: # not blocked on top
                 self.currentState -= 5 # move up
-                self.reward = -1
+            self.reward = -1
 
         elif action == 1: # Right
             if self.currentState not in [2, 4, 5, 9, 12]: # not blocked on right
                 self.currentState += 1 # move right
-                self.reward = -1
+            self.reward = -1
 
         elif action == 2: # Down
             if self.currentState not in [1, 8, 10, 11, 12]: # not blocked on bottom
                 self.currentState += 5 # move down
-                self.reward = -1
+            self.reward = -1
 
         elif action == 3: # Left
             if self.currentState not in [0, 4, 5, 7, 10]: # not blocked on left
                 self.currentState -= 1 # move left
-                self.reward = -1
+            self.reward = -1
 
         else:
             raise ValueError('The requested action is not in actionSpace.')
